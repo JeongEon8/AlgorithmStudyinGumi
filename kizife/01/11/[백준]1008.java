003553e478c62299406ba8ba4a6bb0a16b2a3dbc
kizife <163630533+kizife@users.noreply.github.com>
2024-08-05 01:25:00 +0900

    Create [백준]1008.java

---
+++ kizife/01/11/[백준]1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+        
+        System.out.print(A/B);
+    }
+}
