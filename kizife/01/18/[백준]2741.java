e428aa8a90f088caa6d1d9a890a8243f1dfbf05a
kizife <163630533+kizife@users.noreply.github.com>
2024-08-05 16:05:04 +0900

    Create [백준]2741.java

---
+++ kizife/01/18/[백준]2741.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        
+        for(int i=1; i<=x; i++) {
+            System.out.println(i);
+        }
+
+        
+    }
+}
