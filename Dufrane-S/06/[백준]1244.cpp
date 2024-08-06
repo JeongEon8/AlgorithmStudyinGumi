9466ceaddbd61468175be3ba718f36888c6224dd
Dufrane-S <86463287+Dufrane-S@users.noreply.github.com>
2024-08-06 11:38:15 +0900

    [백준] 06_스위치 켰다 끄기

---
+++ Dufrane-S/06/[백준]1244.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	int n;
+	cin >> n;
+	int box[n];
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		box[i] = a;
+	}
+	int s;
+	cin >> s;
+	for (int i = 0; i < s; i++) {
+		int a,b;
+		cin >> a >> b;
+		if (a == 1) {
+			int c = 1;
+			while (b * c <= n) {
+				box[b*c-1] = !box[b*c-1];
+				c++;
+			}
+		}
+		else {
+			int start = b;
+			int end = b;
+			box[start - 1] = !box[start - 1];
+			start--; end++;
+			while (end-1<n&&start-1>=0&&box[start - 1]== box[end - 1]) {
+				box[start - 1] = !box[start - 1];
+				box[end - 1] = !box[end - 1];
+				start--; end++;
+			}
+		}
+
+	}
+	for (int i = 1; i <= n; i++) {
+		cout << box[i-1] << ' ';
+		if (i % 20== 0 )cout << '\n';
+	}
+}
