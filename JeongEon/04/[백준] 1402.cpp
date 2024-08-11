9d5f3a19403a6bb5ce7d3181b386cfac44f80161
JeongEon <113885866+JeongEon8@users.noreply.github.com>
2024-08-11 17:29:49 +0900

    [백준] 04_아무래도이문제는A번난이도인것같다

---
+++ JeongEon/04/[백준] 1402.cpp
@@ -0,0 +1,19 @@
+// 백준
+// 실버 5
+// 1402. 아무래도이문제는A번난이도인것같다
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		int A, B;
+		cin >> A >> B;
+		cout << "yes" << endl;
+	}
+
+	return 0;
+}
