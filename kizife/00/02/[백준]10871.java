a341ac63642317a51ed75c7ce931984cf004ec47
kizife <163630533+kizife@users.noreply.github.com>
2024-08-12 17:17:18 +0900

    Create [백준]10871.java

---
+++ kizife/00/02/[백준]10871.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      int N = sc.nextInt();
+      int X = sc.nextInt();
+
+      int[] arr = new int[N]; //배열 선언
+
+      for(int i=0; i<N; i++) { //배열 입력
+        arr[i] = sc.nextInt();
+      }
+      
+        for(int i=0; i<N; i++) {
+          if(arr[i]<X) {
+            System.out.print(arr[i] + " ");
+          }
+        }
+
+        
+      }
+
+  }
+
