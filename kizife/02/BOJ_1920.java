748d4dd797a25b8c8925323c07fb4851fa3b78e8
kizizip <163630533+kizizip@users.noreply.github.com>
2025-03-04 13:31:34 +0900

    [백준 - S4] 02_수 찾기

---
+++ kizife/02/BOJ_1920.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+
+        int m = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            if (set.contains(sc.nextInt())) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
