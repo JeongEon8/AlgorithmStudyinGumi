1fd322eb52651bc7658b0199932fd3fb6864207a
kizizip <163630533+kizizip@users.noreply.github.com>
2025-09-01 18:21:48 +0900

    [백준 - S4] 02_상근이의 여행

---
+++ kizizip/02/BOJ_9372.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class BOJ_9372 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            for (int i = 0; i < m; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+            }
+
+            System.out.println(n - 1);
+        }
+    }
+}
