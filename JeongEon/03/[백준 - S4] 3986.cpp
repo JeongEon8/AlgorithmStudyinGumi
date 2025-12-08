a76dfd228e26f51318d3ad4bbcf2ce9fb1f6a500
JeongEon <unionion1388@naver.com>
2025-12-08 19:22:09 +0900

    [백준 - S4] 03_좋은 단어

---
+++ JeongEon/03/[백준 - S4] 3986.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+ 
+int main() {
+    int N;
+    cin >> N;
+ 
+    int ans = 0;
+    while (N--) {
+        stack<char> stack;
+        string word;
+        cin >> word;
+ 
+        for (int i = 0; i < word.length(); i++) {
+            if (stack.empty()) {
+                stack.push(word[i]);
+            }
+            else {
+                if (stack.top() == word[i]) {
+                    stack.pop();
+                }
+                else {
+                    stack.push(word[i]);
+                }
+            }
+        }
+ 
+        if (stack.empty()) {
+            ans++;
+        }
+    }
+ 
+    cout << ans;
+}
