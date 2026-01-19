6dd4b78dbf6cfb28cec80b2614418621a0307726
JeongEon <unionion1388@naver.com>
2026-01-19 17:47:31 +0900

    [백준 - S3] 01_경로 찾기

---
+++ JeongEon/01/[백준 - S1] 11403.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+
+using namespace std;
+
+// 선언
+vector<int>v[100];
+int visit[100];
+
+void dfs(int x){
+    for(int i = 0; i < v[x].size(); i++){
+        if(!visit[v[x][i]]){
+            visit[v[x][i]] = 1;
+            dfs(v[x][i]);
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // 입력부
+    int n, a;
+    cin >> n;
+
+   
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> a;
+            if (a) {
+                v[i].push_back(j);
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        memset(visit,0,sizeof(visit));
+        dfs(i);
+        for (int j = 0; j < n; j++) {
+            cout << visit[j] << " ";
+        }
+
+        cout << '\n';
+    }
+}
