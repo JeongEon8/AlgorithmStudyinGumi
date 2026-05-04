188cbddf40f33d84fc11374fad0f6a23547c0ffd
kizizip <163630533+kizizip@users.noreply.github.com>
2026-05-04 17:04:10 +0900

    [PGS-LV1] 02_두 정수 사이의 합

---
+++ kizizip/02/[PGS-LV1] 02_두 정수 사이의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        
+        for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+            answer += i;
+        }
+        
+        return answer;
+    }
+}
