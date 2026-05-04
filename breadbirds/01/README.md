38bb402a5e35c0c0a5acc696e6b025781aa28d87
정유진 <uuujin0415@gmail.com>
2026-05-04 19:09:48 +0900

    [PGS - LV2] 01_피보나치 수

---
+++ breadbirds/01/README.md
@@ -0,0 +1,17 @@
+# [PGS - lv2] 01\_피보나치 수
+
+## ⏰**time**
+
+30분
+
+## :pushpin: **Algorithm**
+
+구현
+
+## 📡 Link
+
+<https://school.programmers.co.kr/learn/courses/30/lessons/12945>
+
+```
+
+```
---
+++ breadbirds/01/[PGS - lv2] 피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    
+    public int solution(int n) {
+        int[] dp = new int[100001];
+
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) %1234567;
+        }
+        
+        int answer = dp[n];
+        return answer;
+    }
+}
