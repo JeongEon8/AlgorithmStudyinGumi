6f27be27362e450b3977a03bfdba7349910dd180
yujin <uuujin0415@gmail.com>
2026-05-11 18:13:54 +0900

    [PGS - lv1] 03_자릿수 더하기

---
+++ breadbirds/03/README.md
@@ -0,0 +1,17 @@
+# [PGS - lv1] 03\_자릿수 더하기
+
+## ⏰**time**
+
+10분
+
+## :pushpin: **Algorithm**
+
+구현
+
+## 📡 Link
+
+<https://school.programmers.co.kr/learn/courses/30/lessons/12931>
+
+```
+
+```
---
+++ breadbirds/03/[PGS - lv1] 자릿수 더하기.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
