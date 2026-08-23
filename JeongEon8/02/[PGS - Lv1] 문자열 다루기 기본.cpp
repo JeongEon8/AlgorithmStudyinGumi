e26530bfe3675cd54f6a05ad2c3188da719e5e73
JeongEon <unionion1388@naver.com>
2026-08-23 18:49:15 +0900

    [PGS - Lv1] 02_문자열 다루기 기본

---
+++ JeongEon8/02/[PGS - Lv1] 문자열 다루기 기본.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if(s.length() == 4 || s.length() == 6){
+        for(int i = 0; i < s.length(); i++){
+            if(!isdigit(s[i])){
+                answer = false;
+                break;
+            }
+        }
+    }
+    else{
+        answer = false;
+    }
+    
+    return answer;
+}
